5ecf0dd23e2b2caa7db4f6f6b0570f0ace584208
Yi Wang <e@yi-wang.me>
2018-04-08 20:28:56 -0400

    add super

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,27 @@
+mod outermost {
+    pub fn middle_function() {
+        inside::inner_function();
+    }
+
+    fn middle_secret_function() {}
+
+    mod inside {
+        pub fn inner_function() {
+            super::middle_secret_function();
+            ::outermost::middle_secret_function();
+        }
+
+        fn secret_function() {}
+    }
+}
+
+fn try_me() {
+    outermost::middle_function();
+    //outermost::middle_secret_function();
+    //outermost::inside::inner_function();
+    //outermost::inside::secret_function();
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
